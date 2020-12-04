9cb5f8a542f2dc896e7b7695fa83ef63d99dd4fc
abhinavsood123 <ABHINAVSOOD725@GMAIL.COM>
2020-12-04 19:49:10 +0530

    created chainclass

---
+++ chain.js
@@ -0,0 +1,21 @@
+class Chain{
+
+constructor(a,b){
+var options={
+bodyA:a,
+bodyB:b,
+stiffness:0.04,
+length:10,
+
+}
+this.chain=Constraint.create(options);
+World.add(world,this.chain);
+}
+display(){
+strokeWeight(5);
+line (this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y);
+
+}
+
+
+}
